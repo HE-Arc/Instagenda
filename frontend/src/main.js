2be4167be786488ded2e662b1a8e059534b420a0
Naddeo Eddy <eddy.naddeo@he-arc.ch>
2025-02-19 12:25:25 +0100

    Adding quasar config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,18 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app
+.use(Quasar, {
+    plugins: {},
+})
+.use(router)
 
 app.mount('#app')
