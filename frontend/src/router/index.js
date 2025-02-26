247249ffc7b95fe0a6afad40122bbe967ad27abe
Naddeo Eddy <eddy.naddeo@he-arc.ch>
2025-02-26 17:00:19 +0100

    adding proper view name (vuejs)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 import axios from 'axios'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {requiresGuest: true}
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
       meta: {requiresGuest: true}
     },
     {
@@ -24,7 +32,13 @@ const router = createRouter({
       name: 'user',
       component: () => import('../views/UserView.vue'),
       meta: {requiresAuth: true}
-    }
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {requiresAuth: true}
+    },
   ],
 })
 
@@ -35,16 +49,16 @@ router.beforeEach(async (to, from, next) => {
       if (response.status === 200) {
         next();
       } else {
-        next('/');
+        next('/login');
       }
     } catch (error) {
-      next('/');
+      next('/login');
     }
   } else if (to.meta.requiresGuest) {
     try {
       const response = await axios.get('/profile');
       if (response.status === 200) {
-        next('/user');
+        next('/');
       } else {
         next();
       }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,65 +1,10 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { QInput, QBtn } from 'quasar'
-import axios from 'axios'
 import router from '@/router'
-import { updateCSRF } from '@/main'
-
-const username = ref('')
-const password = ref('')
-const username_register = ref('')
-const password_register = ref('')
-
-const status = ref('')
-
-const login = async () => {
-  try {
-    await axios.post('/login', {
-      username: username.value,
-      password: password.value
-    })
-    updateCSRF()
-    router.push('/user')
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-const register = async () => {
-  try {
-    await axios.post('/register', {
-      username: username_register.value,
-      password: password_register.value
-    })
-    updateCSRF()
-    router.push('/user')
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-onMounted(async () => {
-  try {
-    const response = await axios.get('/backend-status')
-    status.value = response.data.status
-  } catch (error) {
-    console.error(error)
-  }
-})
+import { QBtn } from 'quasar'
 </script>
 
 <template>
   <main>
-    <h1>Backend status: {{status}}</h1>
-    <h1>Login</h1>
-
-    <QInput v-model="username" label="Username"/>
-    <QInput v-model="password" label="Password" type="password"/>
-    <QBtn label="Login" @click="login"/>
-
-    <h1>Register</h1>
-    <QInput v-model="username_register" label="Username"/>
-    <QInput v-model="password_register" label="Password" type="password"/>
-    <QBtn label="Register" @click="register"/>
+    <QBtn label="profile" @click="router.push('/user')"/>
   </main>
 </template>
